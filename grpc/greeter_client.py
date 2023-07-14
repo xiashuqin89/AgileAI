b5d740d8f76330bb474adced83c169f18a157b8d
neolei <neolei@tencent.com>
2023-07-14 13:16:21 +0800

    grpc 0.2

--- grpc/greeter_client.py
+++ grpc/greeter_client.py
@@ -6,8 +6,39 @@ import grpc
 import helloworld_pb2
 import helloworld_pb2_grpc
 
+import _credentials
 
-def run():
+_LOGGER = logging.getLogger(__name__)
+_LOGGER.setLevel(logging.INFO)
+
+
+_SERVER_ADDR_TEMPLATE = 'localhost:%d'
+_SIGNATURE_HEADER_KEY = 'x-signature'
+
+
+class AuthGateway(grpc.AuthMetadataPlugin):
+
+    def __call__(self, context, callback):
+        """Implements authentication by passing metadata to a callback.
+
+        Implementations of this method must not block.
+
+        Args:
+          context: An AuthMetadataContext providing information on the RPC that
+            the plugin is being called to authenticate.
+          callback: An AuthMetadataPluginCallback to be invoked either
+            synchronously or asynchronously.
+        """
+        # Example AuthMetadataContext object:
+        # AuthMetadataContext(
+        #     service_url=u'https://localhost:50051/helloworld.Greeter',
+        #     method_name=u'SayHello')
+        signature = context.method_name[::-1]
+        # NOTE: The metadata keys provided to the callback must be lower-cased.
+        callback(((_SIGNATURE_HEADER_KEY, signature),), None)
+
+
+def send_rpc():
     # NOTE(gRPC Python Team): .close() is possible on a channel and should be
     # used in circumstances in which the with statement does not fit the needs
     # of the code.
@@ -16,6 +47,32 @@ def run():
         stub = helloworld_pb2_grpc.GreeterStub(channel)
         response = stub.SayHello(helloworld_pb2.HelloRequest(name='you'))
     print("Greeter client received: " + response.message)
+
+
+def send_rpc_with_auth():
+    call_credentials = grpc.metadata_call_credentials(AuthGateway(),
+                                                      name='auth gateway')
+    channel_credential = grpc.ssl_channel_credentials(_credentials.ROOT_CERTIFICATE)
+    composite_credentials = grpc.composite_channel_credentials(
+        channel_credential,
+        call_credentials,
+    )
+    with grpc.secure_channel('localhost:50051', composite_credentials) as channel:
+        stub = helloworld_pb2_grpc.GreeterStub(channel)
+        request = helloworld_pb2.HelloRequest(name='you')
+        try:
+            response = stub.SayHello(request)
+            print("Greeter client received: " + response.message)
+        except grpc.RpcError as rpc_error:
+            _LOGGER.error('Received error: %s', rpc_error)
+            return rpc_error
+        else:
+            _LOGGER.info('Received message: %s', response)
+            return response
+
+
+def run():
+    send_rpc_with_auth()
 
 
 if __name__ == '__main__':
--- grpc/greeter_server.py
+++ grpc/greeter_server.py
@@ -5,6 +5,29 @@ import grpc
 import helloworld_pb2
 import helloworld_pb2_grpc
 
+import _credentials
+
+_LOGGER = logging.getLogger(__name__)
+_LOGGER.setLevel(logging.INFO)
+
+_LISTEN_ADDRESS_TEMPLATE = 'localhost:%d'
+_SIGNATURE_HEADER_KEY = 'x-signature'
+
+
+class SignatureValidationInterceptor(grpc.ServerInterceptor):
+    def __init__(self):
+        def abort(ignored_request, context):
+            context.abort(grpc.StatusCode.UNAUTHENTICATED, 'Invalid signature')
+        self._abortion = grpc.unary_unary_rpc_method_handler(abort)
+
+    def intercept_service(self, continuation, handler_call_details):
+        method_name = handler_call_details.method.split('/')[-1]
+        expected_metadata = (_SIGNATURE_HEADER_KEY, method_name[::-1])
+        if expected_metadata in handler_call_details.invocation_metadata:
+            return continuation(handler_call_details)
+        else:
+            return self._abortion
+
 
 class Greeter(helloworld_pb2_grpc.GreeterServicer):
 
@@ -17,14 +40,20 @@ class Greeter(helloworld_pb2_grpc.GreeterServicer):
 
 def serve():
     port = '50051'
-    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10),
+                         interceptors=(SignatureValidationInterceptor(),))
     helloworld_pb2_grpc.add_GreeterServicer_to_server(Greeter(), server)
-    server.add_insecure_port('[::]:' + port)
+    server_credentials = grpc.ssl_server_credentials(((
+        _credentials.SERVER_CERTIFICATE_KEY,
+        _credentials.SERVER_CERTIFICATE,
+    ),))
+    # server.add_insecure_port('[::]:' + port)
+    server.add_secure_port('[::]:' + port, server_credentials)
     server.start()
     print("Server started, listening on " + port)
     server.wait_for_termination()
 
 
 if __name__ == '__main__':
-    logging.basicConfig()
+    logging.basicConfig(level=logging.INFO)
     serve()
