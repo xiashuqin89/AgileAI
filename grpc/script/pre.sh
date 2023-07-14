025b1dc8d5439fb556684e3e9b9cc19fcffdc5ab
neolei <neolei@tencent.com>
2023-07-14 20:03:34 +0800

    grpc 0.7

--- grpc/script/pre.sh
+++ grpc/script/pre.sh
@@ -1,4 +1,3 @@
-python -m grpc_tools.protoc -I ../../protos --python_out=. --pyi_out=. --grpc_python_out=. ../../protos/helloworld.proto
 python -m grpc_tools.protoc -I ./protos --python_out=. --pyi_out=. --grpc_python_out=. ./protos/helloworld.proto
 
 docker run -d -p 8500:8500 --restart=always --name=consul consul:1.15.4 agent -server -bootstrap -ui -node=1 -client='0.0.0.0'
