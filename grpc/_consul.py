26e2f89a58e74ce4fa051820690fd1fab5663cbb
neolei <neolei@tencent.com>
2023-07-15 18:09:06 +0800

    grpc 2.0

--- grpc/_consul.py
+++ grpc/_consul.py
@@ -1,10 +1,14 @@
-import socket
+import os
 
 import consul
 
 
+HOST = os.getenv('CONSUL_HOST', '127.0.0.1')
+PORT = int(os.getenv('CONSUL_PORT', 8500))
+
+
 def register(server_name, ip, port: int):
-    c = consul.Consul(host='127.0.0.1', port=8500)  # 连接consul 服务器，默认是127.0.0.1，可用host参数指定host
+    c = consul.Consul(host=HOST, port=PORT)  # 连接consul 服务器，默认是127.0.0.1，可用host参数指定host
     print(f"GRPC开始注册服务{server_name}")
     check = consul.Check.tcp(ip, port, "10s")  # 健康检查的ip，端口，检查时间
     if c.agent.service.register(name=server_name,
@@ -18,7 +22,7 @@ def register(server_name, ip, port: int):
 
 
 def unregister(service_id):
-    c = consul.Consul()
+    c = consul.Consul(host=HOST, port=PORT)
     print(f"开始退出服务{service_id}")
     if c.agent.service.deregister(service_id=service_id):
         print(f"GRPC注销服务{service_id}成功")
@@ -27,7 +31,7 @@ def unregister(service_id):
 
 
 def get_service(server_name):
-    c = consul.Consul(host='127.0.0.1', port=8500)
+    c = consul.Consul(host=HOST, port=PORT)
     _, nodes = c.health.service(service=server_name)
     if len(nodes) == 0:
         raise Exception('service is empty.')
